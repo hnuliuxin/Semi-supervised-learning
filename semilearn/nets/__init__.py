95d103a23d4d46e996102f8d0a0bc39567e9aead
hnuliuxin <1245607800@qq.com>
2024-06-17 10:39:13 +0800

    feat: 添加MobileNet模型的支持

--- semilearn/nets/__init__.py
+++ semilearn/nets/__init__.py
@@ -2,8 +2,10 @@
 # Licensed under the MIT License.
 
 from .resnet import resnet50
-from .wrn import wrn_28_2, wrn_28_8, wrn_var_37_2
+from .wrn import wrn_28_2, wrn_28_8, wrn_var_37_2, wrn_16_1, wrn_16_2, wrn_40_1, wrn_40_2
 from .vit import vit_base_patch16_224, vit_small_patch16_224, vit_small_patch2_32, vit_tiny_patch2_32, vit_base_patch16_96
 from .bert import bert_base_cased, bert_base_uncased
 from .wave2vecv2 import wave2vecv2_base
 from .hubert import hubert_base
+from .mobilenet import mobilenet
+from .shufflenet import ShuffleV1, ShuffleV2
