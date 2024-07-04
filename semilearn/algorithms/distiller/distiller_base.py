2ab7a494b6e30a5333911932ff3c392d227ae3d3
hnuliuxin <1245607800@qq.com>
2024-07-04 11:02:56 +0800

    feat: Add DistillerBase class for distillation algorithm implementation

---
+++ semilearn/algorithms/distiller/distiller_base.py
@@ -0,0 +1,28 @@
+import torch
+from semilearn.core.algorithmbase import AlgorithmBase
+
+class DistillerBase(AlgorithmBase):
+    """
+        Args:
+            - args (`argparse`):
+                algorithm arguments
+            - net_builder (`callable`):
+                network loading function
+            - tb_log (`TBLog`):
+                tensorboard logger
+            - logger (`logging.Logger`):
+                logger to use
+            - T (`float`):
+                Temperature for pseudo-label sharpening
+            - p_cutoff(`float`):
+                Confidence threshold for generating pseudo-labels
+            - hard_label (`bool`, *optional*, default to `False`):
+                If True, targets have [Batch size] shape with int values. If False, the target is vector
+    """
+    def __init__(self, args, net_builder, teacher_net_builder,  tb_log=None, logger=None):
+        super().__init__(args, net_builder, tb_log, logger) 
+        # teacher model init
+        self.teacher_net_builder = teacher_net_builder
+        self.teacher_model = self.teacher_net_builder(num_classes=self.num_classes, pretrained=self.args.use_pretrain, pretrained_path=self.args.pretrain_path)
+        
+    
